a310121067480530f157f6cf366113829167b88a
DavidmB <david.m.bedford1@gmail.com>
2019-03-14 14:15:33 -0400

    mar 14: first push

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// these are general project notes
+// i want to create an onclick function for the images
+// then i want to create a variable that tracks the aount of clicks per image
+// then i want to create a variable that equals the amount of clicks times the value
+// where the value is the randomly assigned number
+
+// use $(".myElementClass").text("to create win/loss messages")
+// you can also add html elements -> with text and classes and all
+// (1) $(".myElementClass").html("<p class=\"myclass\" to create win/loss messages")
+// or
+// (1) var myNewEl = $("<p>")
+// myNewEl.text("check this out")
+// myNewEl.addClass("checkClass")
+// $("myElement").html(myNewEl)
+
+// questions: how to properly link jquery
+// q: how to do bootstrap-js
