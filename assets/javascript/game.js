1355e4aa92ef11e0ffc8afd9610fffd87d5d6210
DavidmB <david.m.bedford1@gmail.com>
2019-03-16 19:57:50 -0400

    mar 16th, 757pm

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,14 @@
-// these are general project notes
-// i want to create an onclick function for the images
-// then i want to create a variable that tracks the aount of clicks per image
-// then i want to create a variable that equals the amount of clicks times the value
-// where the value is the randomly assigned number
-
-// use $(".myElementClass").text("to create win/loss messages")
-// you can also add html elements -> with text and classes and all
-// (1) $(".myElementClass").html("<p class=\"myclass\" to create win/loss messages")
-// or
-// (1) var myNewEl = $("<p>")
-// myNewEl.text("check this out")
-// myNewEl.addClass("checkClass")
-// $("myElement").html(myNewEl)
-
-// questions: how to properly link jquery
-// q: how to do bootstrap-js
-
 $(document).ready(function() {
 // Variables related to scores and misc global data
   var winTotal = 0;
   var lossTotal = 0;
-  var counter = 0; //for calculating how much youve hit so far
+  scoreCounter = 0; //for calculating how much youve hit so far
+  console.log(scoreCounter);
 
 // Random Number Options/Generation
   var targetNumberOptions = [40, 50, 60];
   var targetNumber = targetNumberOptions[Math.floor(Math.random() * targetNumberOptions.length)];
-  console.log(targetNumber);
+  console.log("tar", targetNumber);
 
   var crysOneOptions = [1, 2, 3];
   var crysOneValue = crysOneOptions[Math.floor(Math.random() * crysOneOptions.length)];
@@ -50,10 +33,37 @@ $(document).ready(function() {
 
 //Code for Value Assignment
   $("#targetNumber").attr("data-targetNumber", targetNumber)
-  $("#crystalOne").attr("data-crysOneValue", crysOneValue);
-  $("#crystalTwo").attr("data-crysTwoValue", crysTwoValue);
-  $("#crystalThree").attr("data-crysThreeValue", crysThreeValue);
-  $("#crystalFour").attr("data-crysFourValue", crysFourValue);
+  $("#crystalOne").attr("data-crysValue", crysOneValue);
+  $("#crystalTwo").attr("data-crysValue", crysTwoValue);
+  $("#crystalThree").attr("data-crysValue", crysThreeValue);
+  $("#crystalFour").attr("data-crysValue", crysFourValue);
 
+// On.Click functions for the crystal images
+  $(".crystal-image").on("click", function() {
+    var crystalValue = $(this).attr("data-crysValue");
+      crystalValue = parseInt(crystalValue);
+      scoreCounter += crystalValue;
+      $("#scoreCounter").text(scoreCounter);
+      console.log(scoreCounter);
+
+    });
 
 });
+
+// these are general project notes
+// i want to create an onclick function for the images
+// then i want to create a variable that tracks the aount of clicks per image
+// then i want to create a variable that equals the amount of clicks times the value
+// where the value is the randomly assigned number
+
+// use $(".myElementClass").text("to create win/loss messages")
+// you can also add html elements -> with text and classes and all
+// (1) $(".myElementClass").html("<p class=\"myclass\" to create win/loss messages")
+// or
+// (1) var myNewEl = $("<p>")
+// myNewEl.text("check this out")
+// myNewEl.addClass("checkClass")
+// $("myElement").html(myNewEl)
+
+// questions: how to properly link jquery
+// q: how to do bootstrap-js
